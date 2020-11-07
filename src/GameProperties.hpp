7bb88cda5365b816b6f7d1ba00f3b6f3e29d6ded
Synchronleuchter <x-heiko@skullbyte.de>
2020-11-07 17:01:00 +0100

    Camera scroll tweaking

--- src/GameProperties.hpp
+++ src/GameProperties.hpp
@@ -51,7 +51,7 @@ public:
     static float Zoom() { return 3.0f; }
     static sf::Vector2f ScreenSizeInGame() { return WindowSizeOutGame() / Zoom(); }
 
-    static float ScrollBoundary() { return 60.0f; }
+    static float ScrollBoundary() { return 100.0f; }
     static float ScrollSpeedX() { return 45.0f; }
     static float ScrollSpeedY() { return 70.0f; }
 };
